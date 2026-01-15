e68ab6761235db1d6d112d6e17acf16a71861931
biovaultsoftware <rr.rshemodel@gmail.com>
2026-01-15 12:39:33 +0300

    Delete src directory

--- src/core/hash.ts
+++
@@ -1,5 +0,0 @@
-export async function hashString(data: string): Promise<string> {
-  const encoder = new TextEncoder();
-  const hashBuffer = await crypto.subtle.digest('SHA-256', encoder.encode(data));
-  return Array.from(new Uint8Array(hashBuffer)).map((b) => b.toString(16).padStart(2, '0')).join('');
-}
